20963eb2fc98e796e0d269ebdccc0a15e061d30e
SebMarqz <sebastian_marqz@hotmail.com.ar>
2026-04-13 19:26:41 -0300

    Delete Proyecto vuelos directory

--- Proyecto vuelos/src/Pasajeros_interfaz.java
+++
@@ -1,7 +0,0 @@
-public interface Pasajeros_interfaz {
-    void InicializarLista();
-    void AgregarPasajero(Pasajero pasajero);
-    void EliminarPasajero(int pasaporte);
-    Pasajero BuscarPasajero(int pasaporte);
-    boolean ListaVacia();
-}
